249bf21b39f3aeeccf4e30c9c3d4bcb093bec6ec
karthick-pt6240 <karfthickmtvm@gmail.com>
2023-01-18 12:16:28 +0530

    update

--- src/Manger.java
+++ src/Manger.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Manger extends User implements InventoryControl {
 
     public Manger(String name, String userId, String email, String phone, Account account, UserType userType) {
@@ -24,6 +26,15 @@ public class Manger extends User implements InventoryControl {
                 if (quantity<=item.getQuantity()){
                     float price;
                     price=quantity* item.getItemPrice();
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("if any discount Y/N");
+                    String choice=sc.next();
+                    if(choice.equals("y")){
+                        System.out.println("enter the discount percentage");
+                        float discount=sc.nextFloat();
+                        discount=discount/100;
+                        price=price-(price*discount);
+                    }
                     System.out.println("the total amount rs:"+price);
                     updateQuantity= item.getQuantity()-quantity;
                     Database.updateItem(quantity,updateQuantity,item,this.getUserType(),"sales");
